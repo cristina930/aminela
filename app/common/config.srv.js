63ef496064de0188abd6640cc74ffe10e3c4dfa8
Cristina <csancest@usersad.everis.int>
2016-05-25 15:54:04 +0200

    Con Test y primeros filtros sin conflictos, espero2

--- app/common/config.srv.js
+++ app/common/config.srv.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-function Config() {
-    return{
-=======
+
 function Config (){
     return {
->>>>>>> upstream/master
+
         validation:{
             email: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
         },
@@ -12,14 +9,9 @@ function Config (){
             invalidMail:'Mail mal formado'
         }
     };
-<<<<<<< HEAD
-    
-}
 
-modulo.exports=angular.module('common',[]).constant('config',Config);
-=======
 }
 
 
 module.exports=angular.module('common',[]).constant('config',Config());
->>>>>>> upstream/master
+
